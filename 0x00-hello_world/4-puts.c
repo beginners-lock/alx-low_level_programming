e144a1d8f319cb09d5e5d5ae56fce09644667dad
Chukwu Rophi <chukwurophi@gmail.com>
2022-07-03 14:31:11 -0700

    f

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /*
-** main - Entry function
-** Return: Always return 0
-**/
+*main - Entry function
+*Return: Always return 0
+*/
 
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
