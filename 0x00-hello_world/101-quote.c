a53b297a23f4b530e9ade97453bd17f6962a5ce2
Chukwu Rophi <chukwurophi@gmail.com>
2022-07-03 15:11:42 -0700

    f

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - Entry function
+ * return - always return 1
+ * description - print out quote
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
