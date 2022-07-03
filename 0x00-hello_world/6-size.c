b0442fef62ad77535ab04230cf48df794e3976a1
Chukwu Rophi <chukwurophi@gmail.com>
2022-07-03 14:57:58 -0700

    f

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,13 +11,13 @@ int main(void)
 	char c;
 	int i;
 	long int li;
-	long long ll;
+	long long int ll;
 	float f;
 
 	printf("Size of a char: %d byte(s)\n", sizeof(c));
 	printf("Size of an int: %d byte(s)\n", sizeof(i));
 	printf("Size of a long int: %d byte(s)\n", sizeof(li));
-	printf("Size of a long long : %d byte(s)\n", sizeof(ll));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(ll));
 	printf("Size of a float: %d byte(s)\n", sizeof(f));
 
 	return (0);
