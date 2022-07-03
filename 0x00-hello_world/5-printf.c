4fa39b282768cfd063f5cb7757c88e0406e1d6b7
Chukwu Rophi <chukwurophi@gmail.com>
2022-07-03 14:53:34 -0700

    f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - Entry function
+ * Return - always return 0
+ * Description - prints statement
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the out come is a piece of art,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+ * main - Entry function
+ * Return -always returns 0
+ * Description - print sizes of variables
+ */
+
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long ll;
+	float f;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(c));
+	printf("Size of an int: %d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", sizeof(li));
+	printf("Size of a long long : %d byte(s)\n", sizeof(ll));
+	printf("Size of a float: %d byte(s)\n", sizeof(f));
+
+	return (0);
+}
