494fe6dea9faccc7ad4bebac929d4c467e89667c
Chukwu Rophi <chukwurophi@gmail.com>
2022-07-06 19:18:00 -0700

    f

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,23 +5,23 @@
  * Description: Print first 50 fibonnaci
  * Return: Always 0.
  */
+
 int main(void)
-{ 
-  int a = 1;
-  int b = 2;
-  int sum;
-	
-  printf("%d, %d", a, b);
-  
-  for (int x = 0; x < 50; x++)
-  {
-    sum = a + b;
-    b = a;
-    a = sum;
-    printf(", %d", sum);
-  }
-  
-  printf("\n");
-  return (0);
-  
+{
+	int a = 1;
+	int b = 2;
+	int sum;
+
+	printf("%d, %d", a, b);
+
+	for (int x = 0; x < 50; x++)
+	{
+		sum = a + b;
+		b = a;
+		a = sum;
+		printf(", %d", sum);
+	}
+
+	printf("\n");
+	return (0);
 }
