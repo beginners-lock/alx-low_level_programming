5aeeaf2ec2347187c78c4e9a02a04f435e8b2804
beginners-lock <99103645+beginners-lock@users.noreply.github.com>
2022-07-07 23:55:37 +0100

    f

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - function
+ * @n: line
+ * Description: print lines
+ * Return: void
+ */
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
