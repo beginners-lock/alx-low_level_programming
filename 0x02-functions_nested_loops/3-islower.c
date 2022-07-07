601d57f4220694a9bb930df32814504decaef321
beginners-lock <99103645+beginners-lock@users.noreply.github.com>
2022-07-07 01:49:16 +0100

    f

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - Entry function
+ * @i: This is the ASCII code of the character
+ * Description: checks for lowercases
+ * Return: Always 1 (Success)
+ */
+
+int _islower(int i)
+{
+  if (i >= 97 && i <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
