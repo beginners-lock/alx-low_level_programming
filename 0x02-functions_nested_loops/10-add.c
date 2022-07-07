d0026b71e3ac82da61a380afa1b95fc31de3db4c
beginners-lock <99103645+beginners-lock@users.noreply.github.com>
2022-07-07 02:23:00 +0100

    f

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @x: first integer
+ * @y: second integer
+ * Description: addition of two integers
+ * Return: Result of addition
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
