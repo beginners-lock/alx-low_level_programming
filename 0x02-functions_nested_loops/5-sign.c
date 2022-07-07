1deb579182f4cd665ec67bccdfdbdd01341db0c2
beginners-lock <99103645+beginners-lock@users.noreply.github.com>
2022-07-07 01:56:23 +0100

    f

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry function
+ * @i: number as int
+ * Description: prints the sign of the number
+ * Return: 1 if i is greater than zero, 0 if i is zero, -1 if i is less than zero
+ */
+
+int print_sign(int i)
+{
+	if (i > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (i < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
+}
