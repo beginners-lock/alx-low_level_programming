5c1a13e148a5bd3dbeccdef2c7d120c423fddc54
Chukwu Rophi <chukwurophi@gmail.com>
2022-07-07 13:47:07 -0700

    f

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,32 +1,8 @@
-int _putchar(char k);
-
-void print_alphabet(void);
-
-void print_alphabet_x10(void);
-
-int _islower(int c);
-
-int _isalpha(int c);
-
-int print_sign(int n);
-
-int _abs(int a);
-
-int print_last_digit(int a);
-
-void jack_bauer(void);
-
-void times_table(void);
-
-int add(int a, int b);
-
-void print_to_98(int n);
-
 #ifndef MAIN_H 
  
 #define MAIN_H 
  
-#include <stdio.h> 
+#include <stdio.h>
  
 void positive_or_negative(int i); 
 
