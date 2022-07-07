22c87140d3932b3be8b54dbf9fcfdf70dc477076
Chukwu Rophi <chukwurophi@gmail.com>
2022-07-06 17:11:29 -0700

    f

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints out _putchar+new line
+ * Return: Always returns 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
