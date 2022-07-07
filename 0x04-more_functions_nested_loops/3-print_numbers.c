e1e935676cf598ce87ed58a83ddcfbd6a711499a
beginners-lock <99103645+beginners-lock@users.noreply.github.com>
2022-07-07 23:45:41 +0100

    f

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Function
+ * Decription: Prints from 0 - 9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
