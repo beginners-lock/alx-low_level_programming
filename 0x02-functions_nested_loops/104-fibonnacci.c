34af5c3981d2d8746a18cc73643886ff0f0374d4
beginners-lock <99103645+beginners-lock@users.noreply.github.com>
2022-07-07 03:38:00 +0100

    f

---
+++ 0x02-functions_nested_loops/104-fibonnacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Print first 50 fibonnaci
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int a = 1;
+	long int b = 2;
+	int x;
+	long int sum;
+
+	printf("%ld, %ld", a, b);
+
+	for (x = 0; x < 96; x++)
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+		printf(", %ld", sum);
+	}
+
+	printf("\n");
+	return (0);
+}
