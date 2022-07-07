57c5379563ca39e490d1d0ba85397c1b3c2f1dfd
beginners-lock <99103645+beginners-lock@users.noreply.github.com>
2022-07-07 01:58:18 +0100

    f

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - Entry
+ * @i: as int
+ * Description: prints absolute value of int
+ * Return: absolute value
+ */
+
+int _abs(int i)
+{
+
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
