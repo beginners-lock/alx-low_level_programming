0ec4e138653e159712f04e074b88f24a29b9cf86
beginners-lock <99103645+beginners-lock@users.noreply.github.com>
2022-07-07 23:53:59 +0100

    f

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - Function
+ * Description: prints 0-14
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 15; y++)
+		{
+			if (y >= 10)
+				_putchar((y / 10) + 48);
+			_putchar((y % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
