98d9739b3cf637c9c2d8920657e2038fa42c64c2
beginners-lock <99103645+beginners-lock@users.noreply.github.com>
2022-07-23 00:20:38 +0100

    f

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two string
+ * @dest: destination
+ * @src: source
+ * Return: returns pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int n1 = 0, n2 = 0;
+
+	while (*(dest + n1) != '\0')
+	{
+		n1++;
+	}
+
+	while (n2 >= 0)
+	{
+		*(dest + n1) = *(src + n2);
+		if (*(src + n2) == '\0')
+			break;
+		n1++;
+		n2++;
+	}
+	return (dest);
+}
