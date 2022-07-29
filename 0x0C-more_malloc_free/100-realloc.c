3c096d95c5101ce074378fa2d8921710bf4a66dd
beginners-lock <99103645+beginners-lock@users.noreply.github.com>
2022-07-29 03:29:01 +0100

    f

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,12 +17,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
-  if (ptr == NULL)
+	if (ptr == NULL)
 		ptr = malloc(new_size);
 	if (new_size == old_size)
 		return (ptr);
 
-  free(ptr);
+	free(ptr);
 	ptr = malloc(new_size);
 
 	return (ptr);
