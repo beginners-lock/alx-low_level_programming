0047493e3840d6312f780890300d5bb26fa5e5a2
beginners-lock <99103645+beginners-lock@users.noreply.github.com>
2022-08-03 05:26:36 +0100

    f

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,7 +6,6 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- * Description: creates a new dog instance
  * Return: struct dog
  */
 
@@ -28,8 +27,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (lowner = 0; owner[lowner]; lowner++)
 		;
 
-	p_dog->owner = malloc(lowner + 1);
 	p_dog->name = malloc(lname + 1);
+	p_dog->owner = malloc(lowner + 1);
 
 	if (!(p_dog->name) || !(p_dog->owner))
 	{
@@ -38,16 +37,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(p_dog);
 		return (NULL);
 	}
-
 	for (i = 0; i < lname; i++)
 		p_dog->name[i] = name[i];
+	p_dog->name[i] = '\0';
 
-  	p_dog->name[i] = '\0';
 	p_dog->age = age;
 
 	for (i = 0; i < lowner; i++)
 		p_dog->owner[i] = owner[i];
+	p_dog->owner[i] = '\0';
 
-  	p_dog->owner[i] = '\0';
 	return (p_dog);
 }
