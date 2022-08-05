c55a28ffbf49db030988c69e46b1f108f0ff135d
beginners-lock <99103645+beginners-lock@users.noreply.github.com>
2022-08-05 05:26:34 +0100

    f

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,14 +3,14 @@
 /**
  * print_all - prints anything
  * @format: list of types of argument
- * Description: prints anything
+ * Description: function prints anything
  * Return: void
  */
 
 void print_all(const char * const format, ...)
 {
 	va_list valist;
-	unsigned int i = 0, j, k = 0;
+	unsigned int i = 0, j, c = 0;
 	char *str;
 	const char t_arg[] = "cifs";
 
@@ -20,7 +20,7 @@ void print_all(const char * const format, ...)
 		j = 0;
 		while (t_arg[j])
 		{
-			if (format[i] == t_arg[j] && k)
+			if (format[i] == t_arg[j] && c)
 			{
 				printf(", ");
 				break;
@@ -30,16 +30,16 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 		case 'c':
-			printf("%c", va_arg(valist, int)), k = 1;
+			printf("%c", va_arg(valist, int)), c = 1;
 			break;
 		case 'i':
-			printf("%d", va_arg(valist, int)), k = 1;
+			printf("%d", va_arg(valist, int)), c = 1;
 			break;
 		case 'f':
-			printf("%f", va_arg(valist, double)), k = 1;
+			printf("%f", va_arg(valist, double)), c = 1;
 			break;
 		case 's':
-			str = va_arg(valist, char *), k = 1;
+			str = va_arg(valist, char *), c = 1;
 			if (!str)
 			{
 				printf("(nil)");
