7fafa0ec03a1db89f40cf266e172bbb3603813c5
beginners-lock <99103645+beginners-lock@users.noreply.github.com>
2022-08-05 05:19:40 +0100

    f

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: list of types of argument
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list valist;
+	unsigned int x, y, z = 0;
+	char *c;
+	const char t_arg[] = "cifs";
+
+	va_start(valist, format);
+	while (format && format[x])
+	{
+		y = 0;
+		while (t_arg[y])
+		{
+			if (format[x] == t_arg[y] && z)
+			{
+				printf(", ");
+				break;
+			}
+			y++;
+		}
+		switch (format[x])
+		{
+		case 'c':
+			printf("%c", va_arg(valist, int)), z = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(valist, int)), z = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(valist, double)), z = 1;
+			break;
+		case 's':
+			c = va_arg(valist, char *), z = 1;
+			if (!c)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", c);
+			break;
+		}
+		x++;
+	}
+	printf("\n"), va_end(valist);
+}
