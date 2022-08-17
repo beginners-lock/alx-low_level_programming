4c1883d0b43dafa23b64cbe3d8255824780da285
beginners-lock <99103645+beginners-lock@users.noreply.github.com>
2022-08-17 02:08:46 +0100

    f

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - the number of elements in a linked listint_t list
+ * @h: head of list
+ * Description: shows the number f elements in listint_t list
+ * Return: number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t num_nodes = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		num_nodes++;
+	}
+	return (num_nodes);
+}
