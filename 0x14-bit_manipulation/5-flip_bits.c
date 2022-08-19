0dd58388e71d39b836f2d126b46c44294654e27e
beginners-lock <99103645+beginners-lock@users.noreply.github.com>
2022-08-19 05:45:04 +0100

    f

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,24 +2,25 @@
 
 /**
  * flip_bits - counts the number of bits to change
+ * to get from one number to another
  * @n: first number
  * @m: second number
- * Description: counts the number of bits to change
+ * Description: counts the bits to change
  * Return: number of bits to change
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-  int i, count = 0;
-  unsigned long int current;
-  unsigned long int exclusive = n ^ m;
-  
-  for (i = 63; i >= 0; i--)
-  {
-    current = exclusive >> i;
-    if (current & 1)
-      count++;
-  }
-  
-  return (count);
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = exclusive >> i;
+		if (current & 1)
+			count++;
+	}
+
+	return (count);
 }
