592fa9d0345750b548f78a434e475849a27c6d54
beginners-lock <99103645+beginners-lock@users.noreply.github.com>
2022-08-19 05:48:29 +0100

    f

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,10 +2,8 @@
 
 /**
  * get_endianness - checks if a machine is big or little endian
- * Description: checks if machine is big or little endian
  * Return: 0 for big, 1 for little
  */
-
 int get_endianness(void)
 {
 	unsigned int i = 1;
