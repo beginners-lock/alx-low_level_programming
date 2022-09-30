f9b746d14c221915622e10ff3c4f1f854bb59009
beginners-lock <99103645+beginners-lock@users.noreply.github.com>
2022-09-30 03:28:31 +0100

    first

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list
+ * @h: head of the list
+ * Return: number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int num;
+
+	num = 0;
+
+	if (h == NULL)
+		return (num);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
