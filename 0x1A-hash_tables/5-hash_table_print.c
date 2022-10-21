1a1674cf68341bdf1634a361722a6a4e7946c584
beginners-lock <99103645+beginners-lock@users.noreply.github.com>
2022-10-21 18:49:06 +0100

    first

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -3,10 +3,10 @@
 /**
  * hash_table_print - Prints a hash table.
  * @ht: A pointer to the hash table to print.
+ *
  * Description: Key/value pairs are printed in the order
- * Return: hash values in order
+ *              they appear in the array of the hash table.
  */
- 
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *node;
